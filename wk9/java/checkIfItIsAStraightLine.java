b2dad004e6b2363ab54a1869f4697fe31ae957c4
Whalien3520 <48837275+Whalien3520@users.noreply.github.com>
2024-12-07 08:11:46 -0600

    Update checkIfItIsAStraightLine.java

--- wk9/java/checkIfItIsAStraightLine.java
+++ wk9/java/checkIfItIsAStraightLine.java
@@ -10,7 +10,7 @@ class Solution {
         }
         double m = (double)(coordinates[1][1] - coordinates[0][1]) / (coordinates[1][0] - coordinates[0][0]);
         for(int i = 2; i < coordinates.length; i++) {
-            if(m !=  (double)(coordinates[i][1] - coordinates[i - 1][1]) / (coordinates[i][0] - coordinates[i - 1][0]))
+            if(m != (double)(coordinates[i][1] - coordinates[i - 1][1]) / (coordinates[i][0] - coordinates[i - 1][0]))
                 return false;
         }
         return true;
